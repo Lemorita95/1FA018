12153ace712a96180f595d3e7b69d1af80e53e39
Lemorita95 <leandronmorita@gmail.com>
2026-01-22 13:09:05 +0100

    first commit

--- hand-in-3/Q1.py
+++ hand-in-3/Q1.py
@@ -5,18 +5,29 @@ T_actual = 16 # C
 L_measured = 99.6 # cm
 
 alpha = 0.0005 # cm/cm/C, expansion coefficient
-alpha_uncertainty = 0.0001 # cm/cm/C, expansion coefficient
+uncertainty_alpha = 0.0001 # cm/cm/C, expansion coefficient
 
 ruler_marker = 1 # mm
 
 delta_L = L_measured * alpha * (T_actual - T_calibration)
-print(delta_L)
-print(L_measured + delta_L)
+print()
+print(f'delta_L: {delta_L}')
+print(f'L_prime: L_measured + delta_L')
 
-uncertainty_grading = ruler_marker / 10 # cm
-uncertainty_alpha = alpha_uncertainty * L_measured * abs(T_actual - T_calibration)
-uncertainty_total = uncertainty_grading + uncertainty_alpha
+uncertainty_L = ruler_marker / 10 / 2 # cm
 
-print(uncertainty_grading)
-print(uncertainty_alpha)
-print(uncertainty_total)
+partial_L = 1 + alpha*(T_actual-T_calibration)
+partial_alpha = (T_actual-T_calibration) * L_measured
+
+print()
+print(f'sigma_L: {uncertainty_L}')
+print(f'sigma_alpha: {uncertainty_alpha}')
+print(f'partial_L: {partial_L}')
+print(f'partial_alpha: {partial_alpha}')
+
+uncertainty_total = partial_L * uncertainty_L + partial_alpha * uncertainty_alpha
+uncertainty_total_uncorr = (partial_L**2 * uncertainty_L**2 + partial_alpha**2 * uncertainty_alpha**2)**0.5
+
+print()
+print(f'total uncertainty: {uncertainty_total}')
+print(f'total uncertainty uncorrelated: {uncertainty_total_uncorr}')
